8f2e8628704afa6fbaf5303b731e346262bac6ce
indonesia-tech-community <89242724+indonesia-tech-community@users.noreply.github.com>
2021-08-21 13:58:59 +0700

    Create Bot-Simi.sh

---
+++ Bot-Simi.sh
@@ -0,0 +1,79 @@
+default="\033[00m"
+merah="\033[31m"
+
+
+figlet "SimiSimi"
+echo $merah"Script Ini Untuk Have Fun"
+echo "Silahkan Menikmati Dan Jangan Menjomblo"
+echo "----------------------------------"
+echo "Script By Indonesia Tech Community"
+echo "Website: Coming Soon"
+echo "Github: https://github.com/Indonesia-tech-community/"
+echo "Blog: https://indonesiatechcommunity.blogspot.com/"
+echo "Kalo Ada Masalah Hubungi Nomer Di Bawah Ini : 0895336062247 , 082158568065 , 081312238683 , 082377872810"
+echo "----------------------------------"
+echo "1)Chat Simi Simi Version (1) Terbatas"
+echo "2)Chat Simi Simi Version (2) No Terbatas"
+echo "3)Chat Pacar"
+echo "4)Tentang Script Ini"
+echo "5)Exit"
+read -p "Silahkan Pilih!!: " pil
+echo "----------------------------------"
+if [ $pil = "1" ]:
+  then
+echo -n "Masukan Nama Lo!:"
+read namaLo
+read -p "$namaLo: " pil
+echo "Simi: Hai Juga"
+read -p "$namaLo: " pil
+echo "Simi: Kenapa Lo?"
+read -p "$namaLo: " pil
+echo "Simi: Idiot Lo!"
+read -p "$namaLo: " pil
+echo "Simi: Apa Mau Aku Pukul Apa Cium?"
+read -p "$namaLo: " pil
+echo "Simi: Mau Jadi Pacarku?"
+read -p "$namaLo: " pil
+echo "Simi: Lo Ini Aneh Kontol"
+read -p "$namaLo: " pil
+echo "Simi: Bego"
+read -p "$namaLo: " pil
+echo "Simi: Ah Bodo Tutup"
+read -p "$namaLo: " pil
+echo "Simi: Bodo"
+read -p "$namaLo: " pil
+echo "Simi: Yahahaha"
+read -p "$namaLo: " pil
+echo "Simi: Nama Lo Siapa Wibu?"
+read -p "$namaLo: " pil
+echo "Simi: Huu Gak Jelas Lo Wibu"
+read -p "$namaLo: " pil
+echo "Simi: Yahahaha Wibu"
+read -p "$namaLo: " pil
+while [ true ]
+do
+echo "Stop Chat Nya Ngab Waktunya Terbatas!"
+done
+fi
+if [ $pil = "2" ]:
+  then
+echo -n "Masukan Nama Lo!:"
+read you
+while [ repeat ]
+do
+read -p "$you: " pil
+   echo "Bot: "$(shuf -n1 list.txt)
+done
+fi
+if [ $pil = "3" ]:
+  then
+echo -n "Belum Ada Ngabbb!"
+fi
+if [ $pil = "4" ]:
+  then
+echo -n "Script Ini Hanya Untuk Hiburan Saja Kepada Kaum Jomblo Maupun Nolep Jadi Jangan Baper Ya Tod :v"
+fi
+if [ $pil = "5" ]:
+  then
+echo "Exit"
+fi
